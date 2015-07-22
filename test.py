9d93aec732511b9151ba49849a7cb7a59b153b88
aaronw3373 <aaronw3373@gmail.com>
2015-07-21 23:06:19 -0400

    first part of first section is complete

--- test.py
+++ test.py
@@ -1,21 +1,63 @@
+
+print("starting up...")
 import xlrd
+import xlwt
+from datetime import datetime
+
 data_model = xlrd.open_workbook("data_model.xlsx")
 sheet = data_model.sheets()[0]
-print("model imported");
-lowRange = 7
-upRange = 3376
-data = []
-for i in range(lowRange, upRange):
-  data.append(sheet.row_values(i)[4])
 
-l = len(data);
-res = [];
-for i in range(3300, l):
-  avg = 0
-  for j in range(0, 9):
-    avg += data[i - j]
-  res.append(avg / 10)
-print res
+wb = xlwt.Workbook()
+ws = wb.add_sheet("model")
 
-# print len(data)
-# print sum(data) / len(data)
+print("model imported, parsing data...");
+
+# limits on the data store
+lowerRange = 7
+upperRange = 3384
+
+# parse that data to get a list of workable values
+data1 = []
+data2 = []
+data3 = []
+data4 = []
+def sheetParser(input,low, high, colRead, colWrite, head, output):
+  ws.write(1,colWrite,head)
+  num = 2
+  for i in range(low, high):
+    output.append(input.row_values(i)[colRead])
+    ws.write(num,colWrite,input.row_values(i)[colRead])
+    num += 1
+
+sheetParser(sheet,lowerRange,upperRange,4, 1, "Price Close",data1)
+sheetParser(sheet,lowerRange,upperRange,5, 2, "Price Open",data2)
+sheetParser(sheet,lowerRange,upperRange,6, 3, "Price High",data3)
+sheetParser(sheet,lowerRange,upperRange,7, 4, "Price Low",data4)
+
+print('finding averages...')
+
+# Moving Average Calculator
+def numDayAvg(input, numDays, colWrite, head):
+  ws.write(1,colWrite,head)
+  length = len(input)
+  res = [];
+  num = 2
+  for i in range(0, length):
+    avg = 0
+    count = 0;
+    for j in range(0, numDays):
+      if (i - j) >= 0:
+        count += 1
+        avg += input[i - j]
+    res.append(avg / count)
+    ws.write(num,colWrite,(avg / count))
+    num += 1
+
+numDayAvg(data1, 200, 5, "200 Day Avg")
+numDayAvg(data1, 100, 6, "100 Day Avg")
+numDayAvg(data1, 50, 7, "50 Day Avg")
+numDayAvg(data1, 30, 8, "30 Day Avg")
+numDayAvg(data1, 10, 9, "10 Day Avg")
+
+
+wb.save('end_model.xls')
